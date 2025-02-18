ec82aa62f0aae04b5ccaff7d686471880b4a5ce3
Sean Murphy <sean@iamseanmurphy.com>
2025-02-18 10:18:42 +0000

    4.0.0

--- klarna-payments-for-woocommerce/vendor/composer/installed.php
+++ klarna-payments-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '3.9.2',
-        'version' => '3.9.2.0',
-        'reference' => '4735f7a8ac93f866f142fd7d1f4df3cc770120e3',
+        'pretty_version' => '4.0.0',
+        'version' => '4.0.0.0',
+        'reference' => '3452ca7478a77e68d0e38fe7d401b41eaf9bec44',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '3.9.2',
-            'version' => '3.9.2.0',
-            'reference' => '4735f7a8ac93f866f142fd7d1f4df3cc770120e3',
+            'pretty_version' => '4.0.0',
+            'version' => '4.0.0.0',
+            'reference' => '3452ca7478a77e68d0e38fe7d401b41eaf9bec44',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
