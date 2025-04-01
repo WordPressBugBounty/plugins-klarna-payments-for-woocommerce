970237b46813fb19cb388c74735e1f66215c582a
Sean Murphy <sean@iamseanmurphy.com>
2025-04-01 09:17:39 +0000

    4.1.0

--- klarna-payments-for-woocommerce/blocks/build/payment.asset.php
+++ klarna-payments-for-woocommerce/blocks/build/payment.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-polyfill'), 'version' => 'f7b29d469b124294555f');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-hooks', 'wp-html-entities', 'wp-polyfill'), 'version' => '03b005364c5c4731d53c');
--- klarna-payments-for-woocommerce/vendor/composer/installed.php
+++ klarna-payments-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '4.0.3',
-        'version' => '4.0.3.0',
-        'reference' => '5e6196aa29b079f0a419bc56e67ff668c25ec8aa',
+        'pretty_version' => '4.1.0',
+        'version' => '4.1.0.0',
+        'reference' => '2a5e52a36e0bb61fdc2d462bc35b58b789e8ed84',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '4.0.3',
-            'version' => '4.0.3.0',
-            'reference' => '5e6196aa29b079f0a419bc56e67ff668c25ec8aa',
+            'pretty_version' => '4.1.0',
+            'version' => '4.1.0.0',
+            'reference' => '2a5e52a36e0bb61fdc2d462bc35b58b789e8ed84',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
